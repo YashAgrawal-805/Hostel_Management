d05d4469eac2b77275a42e26966226b23c7f4912
YashAgrawal-805 <123628882+YashAgrawal-805@users.noreply.github.com>
2024-03-03 10:52:07 +0530

    Add files via upload

---
+++ Client/src/components/LandingSite/About/About.jsx
@@ -0,0 +1,50 @@
+import { TeamCard } from "./TeamMember";
+function About() {
+
+  const ayush = {
+    name: "Ayush Singh",
+    designation: "Front-end Engineer",
+    image:
+      "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png",
+  };
+  const anshaj = {
+    name: "Anshaj Prakash",
+    designation: "Backend-end Engineer",
+    image:
+      "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png",
+  };
+  const tejash = {
+    name: "Tejash Pati",
+    designation: "Front End Developer",
+    image:
+      "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png",
+  };
+  const yash = {
+    name: "Yash Raj",
+    designation: "Back End Developer",
+    image:
+      "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png",
+  };
+  const acp = {
+    name: "acp",
+    designation: "orkes",
+    image:
+      "https://w7.pngwing.com/pngs/81/570/png-transparent-profile-logo-computer-icons-user-user-blue-heroes-logo-thumbnail.png",
+  };
+
+  return (
+    <>
+      <h1 className="font-bold text-white text-center text-5xl">
+        Meet Our Team!
+      </h1>
+      <div className="py-20 sm:py-25 flex gap-10 flex-wrap justify-center align-center">
+        <TeamCard member={ayush} />
+        <TeamCard member={tejash} />
+        <TeamCard member={anshaj} />
+        <TeamCard member={yash} />
+        <TeamCard member={acp} />
+      </div>
+    </>
+  );
+}
+export { About };
